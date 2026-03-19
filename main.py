5e2244bee8b4cfcd0435c7ee0821fc3d115cbd12
Tfou3likk <lazercity37@gmail.com>
2026-03-20 00:40:03 +0100

    ajout du système d'authentification et gestion des mdp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import secrets
 import string as s
 import csv
 import sqlite3
+import hashlib
 
 def generer_mdp(taille, majuscule = True, chiffre = True, car_spec = True):
   char = s.ascii_lowercase
@@ -36,7 +37,8 @@ connection.execute("PRAGMA foreign_keys = 1")
 cursor = connection.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS user (id_user INTEGER PRIMARY KEY, 
-               username TEXT);''')
+               email TEXT,
+               mdp_compte TEXT);''')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS mdp (id_mdp INTEGER PRIMARY KEY,
                 mdp TEXT,
@@ -45,7 +47,33 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS mdp (id_mdp INTEGER PRIMARY KEY,
                 FOREIGN KEY (id_user) REFERENCES user (id_user)); ''')
 connection.commit()
 
-print(connection.total_changes)
+def hash_mdp(mdp):
+  return  hashlib.sha256(mdp.encode()).hexdigest()
+def create_user(email, mdp_compte):
+  cursor.execute("INSERT INTO user (email, mdp_compte) values (?, ?);", (email, hash_mdp(mdp_compte)))
+  connection.commit()
+
+def login(email, mdp_compte):
+  cursor.execute("SELECT * FROM user WHERE email = ? AND mdp_compte = ?", (email, hash_mdp(mdp_compte)))
+  row =  cursor.fetchone()
+  if row != None:
+    return row[0]
+  else:
+    return False
+  
+def add_mdp(mdp, location, id_user):
+  cursor.execute("INSERT INTO mdp (mdp, location, id_user) VALUES (?, ?, ?)", (mdp, location, id_user))
+  connection.commit()
+
+def get_mdp(location, id_user):
+  cursor.execute("SELECT mdp FROM mdp WHERE location = ? AND id_user = ? ", (location, id_user))
+  row =  cursor.fetchone()
+  if row != None:
+    return row[0]
+  else:
+    return False
+
+#print(connection.total_changes)
 
 
 
